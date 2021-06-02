648f30a6ef54b8aa502dac332a13f9a6ea0b9ae4
mcroche611 <mcroche@ucm.es>
2021-06-02 17:25:30 +0200

    Añadidas clases para pintar barra de turn y leyenda

---
+++ Transistor/CaptionDisplay.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transistor
+{
+
+    /*
+     *  Ejemplo de uso
+     *  
+     
+     CaptionDisplay captionDisplay= new CaptionDisplay(field.numRows, field.numCols);
+
+
+     // Dentro del bucle de juego
+     captionDisplay.Show();
+          
+     * 
+     */
+
+
+
+    class CaptionDisplay
+    {
+        private int topRow;
+        private int maxColumns;
+        Player player = new Player(null, null);
+        Creep creep= new Creep(null, null);
+        Snapshot snapshot= new Snapshot(null, null);
+        Jerk jerk = new Jerk(null, null);
+        Fetch fetch = new Fetch(null, null);
+        Beam beam = new Beam(null, null);
+        Laser laser = new Laser(null, null);
+        Shot shot= new Shot(null, null);
+        Load load = new Load(null, null);
+        Bullet bullet = new Bullet(null, null);
+
+
+        public CaptionDisplay(int topRow, int maxColumns)
+        {
+            this.topRow = topRow;
+            this.maxColumns = maxColumns * 2;
+            
+        }
+
+        public void Show()
+        {
+            string header = new string(' ', 35);
+            int row = 1;
+            int initcol = maxColumns + 1;
+
+            int col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+
+            // Fila 1
+            row++;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+            col++;
+            Console.SetCursorPosition(col, row);
+            SetColor(player.Color);
+            Console.Write(player.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Red (Player)");
+
+            col += 15;
+            Console.SetCursorPosition(col, row);
+            SetColor(creep.Color);
+            Console.Write(creep.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Creep");
+
+            //separador
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+
+            // Fila 2
+            row++;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+            col++;
+            Console.SetCursorPosition(col, row);
+            SetColor(snapshot.Color);
+            Console.Write(snapshot.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Snapshot");
+
+            col += 15;
+            Console.SetCursorPosition(col, row);
+            SetColor(jerk.Color);
+            Console.Write(jerk.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Jerk");
+
+
+            //separador
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+
+            // Fila 3
+            row++;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+            col++;
+            Console.SetCursorPosition(col, row);
+            SetColor(fetch.Color);
+            Console.Write(fetch.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Fetch");
+
+            col += 15;
+            Console.SetCursorPosition(col, row);
+            //SetColor(beam.Color);
+            SetColor(ConsoleColor.Cyan);
+            Console.Write(beam.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Beam");
+
+            //separador
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+
+            // Fila 4
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+            col++;
+            Console.SetCursorPosition(col, row);
+            //SetColor(laser.Color);
+            SetColor(ConsoleColor.Green);
+            Console.Write(laser.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Laser");
+
+            col += 15;
+            Console.SetCursorPosition(col, row);
+            //SetColor(beam.Color);
+            SetColor();
+            Console.Write(shot.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Shot");
+
+            //separador
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+            // Fila 5
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+            col++;
+            Console.SetCursorPosition(col, row);
+            //SetColor(laser.Color);
+            SetColor(ConsoleColor.DarkMagenta);
+            Console.Write(load.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Load");
+
+            col += 15;
+            Console.SetCursorPosition(col, row);
+            //SetColor(beam.Color);
+            SetColor();
+            Console.Write(bullet.Symbols);
+            col += 4;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White, ConsoleColor.Black);
+            Console.Write("Bullet");
+
+            //separador
+            row++;
+            col = initcol;
+            Console.SetCursorPosition(col, row);
+            SetColor(ConsoleColor.White);
+            Console.Write(header);
+
+        }
+        void SetColor(ConsoleColor backColor = ConsoleColor.Black, ConsoleColor foreColor = ConsoleColor.White)
+        {
+            Console.BackgroundColor = backColor;
+            Console.ForegroundColor = foreColor;
+        }
+    }
+}
---
+++ Transistor/TurnDisplay.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transistor
+
+
+/*
+  *  Ejemplo de uso
+  *  
+
+  TurnDisplay turnDisplay = new TurnDisplay(field.numRows, field.numCols);
+  float turnPercentage = 100f;
+
+  // Dentro del bucle de juego
+  turnDisplay.Show(TurnMode.Normal, turnPercentage, 2,4,0,3);
+  turnPercentage -= 0.5f;
+  if (turnPercentage <= 0)
+      turnPercentage = 100;
+
+
+  * 
+  */
+
+{
+    class TurnDisplay
+    {
+        private int topRow;
+        private int maxColumns;
+        int TurnLength = 20;
+
+        public TurnDisplay(int topRow, int maxColumns)
+        {
+            this.topRow = topRow;
+            this.maxColumns = maxColumns*2;
+            TurnLength =(int) (this.maxColumns * 0.7f);
+        }
+        public void Show(TurnMode mode, float turnPercentage, int crash, int breach, int load, int bounce)
+        {
+
+            int row = topRow + 1;
+            int col = 0;
+
+            //row = 1;
+            //col = maxColumns + 1;
+
+            ///// Pintar barra de Turn
+            ///
+
+            Console.SetCursorPosition(col, row);
+            string header = new string(' ', maxColumns);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(header);
+            row++;
+            col += 5;
+            Console.SetCursorPosition(col, row);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Turn:");
+            col += 5;
+            Console.SetCursorPosition(col, row);
+            int turnChars = (int )(TurnLength * turnPercentage/100);
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write(new string(' ',turnChars)); 
+            Console.BackgroundColor = ConsoleColor.Black;
+            int resto = maxColumns - turnChars - col;
+            if (resto <= 0)
+                resto = 1;
+            Console.Write(new string(' ', resto));
+            row++;
+            Console.SetCursorPosition(col, row);
+            SetColor();
+            Console.Write(header);
+            
+            // Pintar ataques
+            ///////////////////////////////////
+            row++;
+            col = 5;
+
+            string label;
+            
+            SetColor(ConsoleColor.DarkYellow, ConsoleColor.Black);
+            Console.SetCursorPosition(col, row);
+            label = "Crash: " + crash;
+            Console.Write(label);
+            col += label.Length + 2;
+
+            SetColor(ConsoleColor.Cyan, ConsoleColor.Black);
+            Console.SetCursorPosition(col, row);
+            label = "Breach: " + breach;
+            Console.Write(label);
+            col += label.Length + 2;
+
+            SetColor(ConsoleColor.Magenta, ConsoleColor.Black);
+            Console.SetCursorPosition(col, row);
+            label = "Load: " + load;
+            Console.Write(label);
+            col += label.Length + 2;
+
+            SetColor(ConsoleColor.DarkGreen, ConsoleColor.Black);
+            Console.SetCursorPosition(col, row);
+            label = "Bounce: " + bounce;
+            Console.Write(label);
+            col += label.Length + 2;
+
+
+
+
+
+
+        }
+        void SetColor(ConsoleColor backColor= ConsoleColor.Black, ConsoleColor foreColor = ConsoleColor.White)
+        {
+            Console.BackgroundColor = backColor;
+            Console.ForegroundColor = foreColor;
+        }
+    }
+}
